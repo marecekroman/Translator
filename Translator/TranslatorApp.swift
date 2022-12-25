291ea380b7581b7fcbacc2ebceba11a3e52c860c
romanmarecek <r_marecek@utb.cz>
2022-12-25 13:16:41 +0100

    Initial Commit

---
+++ Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Roman Mareček on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
